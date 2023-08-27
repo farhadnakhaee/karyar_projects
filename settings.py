cd78d64596ea275f43e8815e5469c38005ecd9ec
Farhad <nakhaie.farhad@gmail.com>
2023-08-27 14:02:20 +0330

    add bullet file and _fire_bullet method in alien_invasion.py

--- settings.py
+++ settings.py
@@ -8,3 +8,8 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (50, 50, 50)
         self.ship_speed = 1
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+
