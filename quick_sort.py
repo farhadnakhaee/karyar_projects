aa217a3ed0342ff8027d104543e0012488c50cd2
Farhad <138935715+farhadnakhaee@users.noreply.github.com>
2023-11-02 11:28:46 +0330

    Create quick_sort.py

---
+++ quick_sort.py
@@ -0,0 +1,21 @@
+def quicksort(array):
+    if not array:
+        return []
+    if len(array) == 1:
+        return array
+    c1, c2 = 0, len(array) - 1
+    pivot = array[-1]
+    while c1 < c2:
+        if array[c1] > pivot:
+            array[c2], array[c2 - 1] = array[c2 - 1], array[c2]
+            c2 -= 1
+            pivot = array[c2]
+            if c2 - c1 > 0:
+                array[c1], array[c2 + 1] = array[c2 + 1], array[c1]
+        else:
+            c1 += 1
+    return quicksort(array[:c2]) + [pivot] + quicksort(array[c2+1:])
+
+
+test = [21, 4, 1, 3, 9, 20, 25, 6, 21, 14]
+print(quicksort(test))
