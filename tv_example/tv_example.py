47a88364bd91da29e0451f37ed2f5f75394e37de
Farhad <138935715+farhadnakhaee@users.noreply.github.com>
2023-11-13 17:17:44 +0330

    remove decorator

--- tv_example/tv_example.py
+++ tv_example/tv_example.py
@@ -1,28 +1,3 @@
-# decorator for volume size
-def volume_check(method):
-    def wrapper(self):
-
-        if 0 < self.volume < self.MAX_VOLUME:
-            method(self)
-        else:
-            print("Volume can't change. (max or min level)")
-
-        return self
-    return wrapper
-
-
-def channel_check(method):
-    def wrapper(self):
-
-        if 0 < self.currentChannelIndex < len(self.channelsList):
-            method(self)
-        else:
-            print("Channel is not in channel list")
-
-        return self
-    return wrapper
-
-
 class TV:
     def __init__(self):
         self.isOn = False
@@ -34,31 +9,33 @@ class TV:
 
     @staticmethod
     def search_channel():
-        # TODO: Get from CABLE!
         return [1, 2, 3, 4, 5, 6, 7, 8, 9, 20, 21, 25]
 
     def power(self):
         self.isOn = not self.isOn
 
-    @volume_check
     def volume_up(self):
-        self.volume += 1
+        if self.volume < self.MAX_VOLUME:
+            self.volume += 1
 
-    @volume_check
     def volume_down(self):
-        self.volume -= 1
+        if self.volume > 0:
+            self.volume -= 1
 
-    @channel_check
     def channel_up(self):
-        self.currentChannelIndex += 1
+        if self.currentChannelIndex < len(self.channelsList)-1:
+            self.currentChannelIndex += 1
 
-    @channel_check
     def channel_down(self):
-        self.currentChannelIndex -= 1
+        if self.currentChannelIndex > 0:
+            self.currentChannelIndex -= 1
 
     def set_channel(self):
         channel = self.input_channel()
-        self.currentChannelIndex = self.channelsList.index(channel)
+        if channel in self.channelsList:
+            self.currentChannelIndex = self.channelsList.index(channel)
+        else:
+            print("Channel isn't in list.")
 
     @staticmethod
     def input_channel():
@@ -70,8 +47,8 @@ class TV:
         print(f"current channel: {self.channelsList[self.currentChannelIndex]}")
 
 
-tv = TV()
-tv.currentChannelIndex = 1
-tv.channel_down()
-tv.channel_down()
-print(f"index: {tv.currentChannelIndex}   channel: {tv.channelsList[tv.currentChannelIndex]}")
+if __name__ == "__main__":
+    tv = TV()
+    tv.set_channel()
+    tv.volume_up()
+    tv.show_info()
