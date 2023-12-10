f3dd4f28af47ca1e46db984969766bd31700e9ba
Farhad <nakhaie.farhad@gmail.com>
2023-12-10 11:03:46 +0330

    update project: add me

--- chatroom/client.py
+++ chatroom/client.py
@@ -1,32 +1,41 @@
 import socket
 import threading
-#Broadcast
+
 
 class ChatClient:
-    def __init__(self, server_ip:str, server_port:int):
-        self.server_socket_address = (server_ip, server_port)
-        
-    def connect(self):
+    def __init__(self, server_ip: str, server_port: int):
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket_address = (server_ip, server_port)
+        self.name = input("name: ")
+
+    def connect(self):
         self.server_socket.connect(self.server_socket_address)
 
     def sending_loop(self):
         while True:
             message = input()
-            self.server_socket.send(message.encode())
+            _message = self.name + ": " + message
+            self.server_socket.send(_message.encode())
 
     def receiving_loop(self):   
         while True:   
             data = self.server_socket.recv(1024)
             if not data:
                 break
-            print(data.decode())
+            split_data = data.decode().split(":")
+            name = split_data[0]
+            if name == self.name:
+                data = "me: " + split_data[1]
+                print(data)
+            else:
+                print(data.decode())
 
     def start(self):
         self.connect()
         sending_loop = threading.Thread(target=self.sending_loop)
         sending_loop.start()
         self.receiving_loop()
+
 
 client = ChatClient("127.0.0.1", 54321)
 client.start()
--- chatroom/server.py
+++ chatroom/server.py
@@ -1,12 +1,9 @@
 import socket
 import threading
 
-# Chat Server
-# Broadcast every message to others.
-# S ---- c1
-# S ---- c2
+
 class ChatServer:
-    def __init__(self, ip:str, port:int):
+    def __init__(self, ip: str, port: int):
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.bind((ip, port))
         self.server_socket.listen()
@@ -17,7 +14,7 @@ class ChatServer:
         print(f"client {addr} is connected")
         return conn
     
-    def clientHandler(self, conn:socket):
+    def handle_client(self, conn: socket):
         while True:
             data = conn.recv(1024)
             if not data:
@@ -26,16 +23,17 @@ class ChatServer:
             # DO SOME OTHER STUFF HERE
             self.broadcast(data)
 
-    def broadcast(self, data:bytes)->None:
+    def broadcast(self, data: bytes) -> None:
         for client in self.clients:
             client.send(data)
 
-    def start(self)->None:
+    def start(self) -> None:
         while True:
             client = self.accept()
             self.clients.append(client)
-            clientHandlerThead =threading.Thread(target=self.clientHandler, args=(client,))
-            clientHandlerThead.start()
+            client_handler_thread = threading.Thread(target=self.handle_client, args=(client,))
+            client_handler_thread.start()
+
 
 server = ChatServer("127.0.0.1", 54321)
 server.start()
